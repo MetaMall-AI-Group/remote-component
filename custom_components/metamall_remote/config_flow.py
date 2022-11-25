fe6acdee1841accf0747cd06d03eb40cd9f4820d
imdgr886 <thrall.chen@gmail.com>
2022-11-25 20:20:29 +0800

    Create config_flow.py

--- custom_components/metamall_remote/config_flow.py
+++ custom_components/metamall_remote/config_flow.py
@@ -1,6 +1,6 @@
 from homeassistant import config_entries
 import voluptuous as vol
-from config.custom_components.metamall_remote.const import DOMAIN
+from .const import DOMAIN
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
